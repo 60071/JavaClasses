0712f34c6f40e82f6e7327ac7f67b308f3adefb6
60071 <qwe60071@gmail.com>
2017-07-27 21:27:41 +0800

    test

---
+++ P001/src/Helloer.java
@@ -0,0 +1,20 @@
+/**
+ * 實作時間記錄：
+ * 日期：<mm/dd>: <00:00>~<00:00>: <00:00>
+ * 程式目標：
+ * 設計規格：
+ * 設計想法：
+ * 虛擬碼：
+ *  
+ */
+
+/**
+ * @author 123
+ *
+ */
+public class Helloer {
+	public void sayHello() {
+		System.out.println("I'm a helloer");
+	}
+
+}
---
+++ P001/src/Runner.java
@@ -0,0 +1,27 @@
+/**
+ * 實作時間記錄：
+ * 日期：<mm/dd>: <00:00>~<00:00>: <00:00>
+ * 程式目標：
+ * 設計規格：
+ * 設計想法：
+ * 虛擬碼：
+ *  
+ */
+
+/**
+ * @author 123
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		Helloer helloer = new Helloer();
+		helloer.sayHello();
+
+	}
+
+}
